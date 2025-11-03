a124433d120e6dcf1a6ff0a47f0cedfa0601e594
yesmine <yesmine.louhichi@enis.tn>
2025-11-03 13:09:20 +0100

    Création d'une opération multiplication pour un projet de calculatrice

---
+++ Operation.java
@@ -0,0 +1,18 @@
+public class Operation {
+    
+    /**
+     * Méthode statique qui multiplie x par y
+     * @param x premier nombre
+     * @param y deuxième nombre  
+     * @return le produit de x et y
+     */
+    public static int multiplier(int x, int y) {
+        return x * y;
+    }
+    
+    // Méthode main pour tester
+    public static void main(String[] args) {
+        int resultat = multiplier(5, 3);
+        System.out.println("5 * 3 = " + resultat);
+    }
+}
