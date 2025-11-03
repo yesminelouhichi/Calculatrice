5f8e86ac3d29971931700b5926e8a1cde233f85c
yesmine <yesmine.louhichi@enis.tn>
2025-11-03 15:06:56 +0100

    Ajout d'un message au revoir

--- Main.java
+++ Main.java
@@ -3,16 +3,27 @@ public class Main {
         int a = 2;
         int b = 3;
         System.out.println("Bienvenue");
+        
         // Test multiplication
         int resultatMult = Operation.multiplier(a, b);
         System.out.println(a + " * " + b + " = " + resultatMult);
-        
+
         // Test soustraction
         int resultatSous = Operation.soustraire(a, b);
         System.out.println(a + " - " + b + " = " + resultatSous);
-        
+
         // Test addition
         int resultatAdd = Operation.additionner(a, b);
         System.out.println(a + " + " + b + " = " + resultatAdd);
+        
+        // Test division
+        try {
+            double resultatDiv = Operation.diviser(a, b);
+            System.out.println(a + " / " + b + " = " + resultatDiv);
+        } catch (ArithmeticException e) {
+            System.out.println("Erreur : " + e.getMessage());
+        }
+        
+        System.out.println("Au revoir");
     }
 }
